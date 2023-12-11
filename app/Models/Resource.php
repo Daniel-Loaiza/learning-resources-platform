656032048ef6c6726c47c4e8bb355c8aebaaf6e8
Daniel-Loaiza <danielloaizaisi@gmail.com>
2023-12-11 00:03:56 -0500

    Updated project up to step 3

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -11,6 +11,6 @@ class Resource extends Model
     protected $fillable = ['title','category_id','creator_id','link','description'];
 
     public function category(){
-        return $this->belongsTo(Category::class);php artisan serve
+        return $this->belongsTo(Category::class);
     }
 }
